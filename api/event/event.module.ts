05c04559d5cc2148f258ae70991d00528df2af74
ClemEshn <contact.klems@gmail.com>
2025-04-19 20:44:53 +0200

    fixed a bug

--- api/event/event.module.ts
+++ api/event/event.module.ts
@@ -3,11 +3,12 @@ import { EventService } from './event.service';
 import { EventController } from './event.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
+import { PriceHistory } from 'price-history/entities/price-history.entity';
 
 @Module({
   controllers: [EventController],
   providers: [EventService],
-  imports: [TypeOrmModule.forFeature([Event])],
+  imports: [TypeOrmModule.forFeature([Event, PriceHistory])],
 
 })
 export class EventModule {}
